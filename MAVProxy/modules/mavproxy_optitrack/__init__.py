bb32a38f201bb6a7ba52ef0ae7fac338c5a2c6fc
ASZ <aastefan@caltech.edu>
2023-03-31 18:23:32 -0700

    checkpoint

--- MAVProxy/modules/mavproxy_optitrack/__init__.py
+++ MAVProxy/modules/mavproxy_optitrack/__init__.py
@@ -27,6 +27,8 @@ class optitrack(mp_module.MPModule):
         self.streaming_client.rigidBodyListener = self.receive_rigid_body_frame
         self.last_msg_time = 0
         self.started = False
+        self.last_print_time = None
+        self.first_debug_print = True
 
     # This is a callback function that gets connected to the NatNet client.
     # It is called once per rigid body per frame
@@ -34,13 +36,23 @@ class optitrack(mp_module.MPModule):
     def receive_rigid_body_frame(self, new_id, position, rotation):#, tracking_valid):
         if (new_id == self.optitrack_settings.obj_id):
             now = time.time()
+            if self.first_debug_print:
+                print(new_id, [round(pos,4) for pos in position] ,\
+                    [round(rot, 4) for rot in rotation])
+                self.first_debug_print = False
             if (now - self.last_msg_time) > (self.optitrack_settings.msg_intvl_ms * 0.001):
                 time_us = int(now * 1.0e6)
                 self.master.mav.att_pos_mocap_send(time_us,
                     (rotation[3], rotation[0], rotation[2], -rotation[1]),
                     position[0], position[2], -position[1])
-                print(time_us, position[0], position[2], -position[1])
                 self.last_msg_time = now
+                """
+                if self.last_print_time is None or (now - self.last_print_time) > 0.5:
+                    print(new_id, [round(pos,4) for pos in position] ,\
+                        [round(rot, 4) for rot in rotation])
+                    self.last_print_time = now
+                """
+                
 
     def usage(self):
         '''show help on command line options'''
